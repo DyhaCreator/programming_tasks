1a1744b15539eb718b3c0ea6362d8bed59719fa4
DyhaCreator <andropet228@gmail.com>
2023-10-17 09:33:05 -0400

    new

---
+++ tinkoff/pref_twopointers/h.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <set>
+#define ll unsigned long long
+
+using namespace std;
+
+int main() {
+    set<ll>lng;
+    int n;
+    cin >> n;
+    int j = 0;
+    for (int i = 0; lng.size() < n + 1; i++) {
+        ll a = i * i * i;
+        lng.insert(a);
+        while (j * j < a) {
+            ll b = j * j;
+            lng.insert(b);
+            j++;
+        }
+    }
+    /*for (auto i : lng) {
+        cout << i << " ";
+    }
+    cout << endl;*/
+    set<ll>::iterator it = lng.begin();
+    advance(it, n);
+    ll x = *it;
+    cout << x << endl;
+    return 0;
+}
