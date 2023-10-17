07eda4b29924af86a43ed452ee6e8328dd36f90f
DyhaCreator <andropet228@gmail.com>
2023-10-17 12:15:35 -0400

    new

--- tinkoff/pref_twopointers/a.cpp
+++ tinkoff/pref_twopointers/a.cpp
@@ -7,24 +7,20 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<ll>vec(n);
+    vector<ll>vec(n + 1);
     ll ans = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> vec[i];
-        bool b = false;
-        for (int j = 0; j < i; j++) {
-            if (vec[j] == vec[i]) {
-                ans -= vec[i];
-                b = true;
-                break;
-            }
-        }
-        if (!b) {
-            ans += vec[i];
-        }
-        //cout << ans << endl;
+    for (int i = 1; i < n + 1; i++) {
+        int x;
+        cin >> x;
+        vec[i] = vec[i - 1] ^ x;
     }
-    cout << ans << endl;
+    cout << vec.back() << endl;
     
     return 0;
 }
+/*
+
+7
+5 7 4 8 5 8 7
+
+*/
