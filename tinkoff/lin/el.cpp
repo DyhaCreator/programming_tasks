fb5adab9c52e6bb740ea8fb7dbe342f02a6ba285
DyhaCreator <andropet228@gmail.com>
2023-11-10 07:41:34 -0500

    new

--- tinkoff/lin/el.cpp
+++ tinkoff/lin/el.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 #define ll long long
 using namespace std;
 
@@ -16,15 +17,36 @@ int main() {
     for (int i = 0; i + k <= n; i++) {
         pref[i] = sum[i + k] - sum[i];
     }
-    for (auto x : pref)
-        cout << x << " ";
-    cout << endl;
-    int i = 2;
-    cout << sum[i] - sum[i - k] << endl;
-    
+    deque<ll>d;
+    for (int j = k; j + k <= n; j++) {
+        while (!d.empty() && pref[d.back()] <= pref[j]) {
+            d.pop_back();
+        }
+        d.push_back(j);
+    }
+    ll min = 1e18 + 10;
+    for (int i = 1; i + k <= n + 1; i++) {
+        ll max = pref[d.front()];
+        if (!d.empty() && d.front() < i + k && d.front() > i - k) {
+            d.pop_front();
+        }
+        if (i >= k) {
+            while (!d.empty() && pref[d.back()] <= pref[i - k]) {
+                d.pop_back();
+            }
+            d.push_back(i - k);
+        }
+        if (max < min) {
+            min = max;
+        }
+    }
+    cout << min << endl;
     return 0;
 }
 /*
+
+10 2
+1 2 4 5 2 4 2 2 1 6
 
 6 2
 4 2 2 3 1 3
