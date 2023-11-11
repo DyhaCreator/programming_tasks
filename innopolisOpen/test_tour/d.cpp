a554f70d1df8f9e926ffedb340194e7cda833808
DyhaCreator <andropet228@gmail.com>
2023-11-11 12:59:40 -0500

    new

--- innopolisOpen/test_tour/d.cpp
+++ innopolisOpen/test_tour/d.cpp
@@ -3,12 +3,44 @@
 #define ll long long
 using namespace std;
 
+int n;
+vector<vector<int>>w;
+
+ll min_sum = 1e18 + 10;
+
+void rec(vector<int>l, int index, int cindex, ll sum) {
+    if (index == cindex) {
+        min_sum = min(sum, min_sum);
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (l[i] == 0) {
+            l[i] = 1;
+            rec(l, i, cindex, sum + w[index][i]);
+            l[i] = 0;
+        }
+    }
+}
+
 int main() {
-    int n;
     cin >> n;
-    vector<vector<int>>a = vector<vector<int>>(n, vector<int>(n));
-    for (auto &x : a)
-        cin >> x;
-    
+    w = vector<vector<int>>(n, vector<int>(n));
+    for (auto &x : w)
+        for (auto &y : x)
+            cin >> y;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                min_sum = 1e18 + 10;
+                vector<int>a = vector<int>(n, 0);
+                a[i] = 1;
+                rec(a, i, j, 0);
+                cout << min_sum << " ";
+            } else {
+                cout << 0 << " ";
+            }
+        }
+        cout << "\n";
+    }
     return 0;
 }
