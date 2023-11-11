88758838ae4a16aac654238657cb9ec3a5965ecb
DyhaCreator <andropet228@gmail.com>
2023-11-11 11:21:13 -0500

    new

---
+++ tinkoff/perebor/e.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int nut, k;
+
+int a[100] = {0};
+
+void rec(int n, int pos) {
+    
+}
+
+int main() {
+    cin >> nut >> k;
+    rec(nut, 0);
+    return 0;
+}
