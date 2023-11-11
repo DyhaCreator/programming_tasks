081124f4c007352efc3d80f46698695164d430ea
DyhaCreator <andropet228@gmail.com>
2023-11-11 11:46:39 -0500

    new

--- innopolisOpen/test_tour/b.cpp
+++ innopolisOpen/test_tour/b.cpp
@@ -7,9 +7,12 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<int>a = vector<int>(n);
+    vector<ll>a = vector<ll>(n);
     for (auto &x : a)
         cin >> x;
     reverse(a.begin(), a.end());
+    for (auto x : a)
+        cout << x << " ";
+    cout << endl;
     return 0;
 }
