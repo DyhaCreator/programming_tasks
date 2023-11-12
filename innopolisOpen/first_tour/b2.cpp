3be791aa884acee70bd9a378d8bd8d28e7041394
DyhaCreator <andropet228@gmail.com>
2023-11-12 15:04:14 +0500

    new

--- innopolisOpen/first_tour/b2.cpp
+++ innopolisOpen/first_tour/b2.cpp
@@ -3,15 +3,16 @@
 #include <vector>
 #include <stack>
 #include <deque>
+#include <set>
+#include <unordered_set>
 #define ll long long
 using namespace std;
 
-ll n, x, y;
+int n, x, y;
 vector<ll>days;
 
 bool f(ll day) {
     for (int i = 0; i < n; i++) {
-        //cout << days[i] << " " << fase(days[i], day) << endl;
         if ((days[i] - day + 1) % (x + y) > x || (days[i] - day + 1) % (x + y) == 0) {
             return false;
         }
@@ -24,21 +25,19 @@ int main() {
     days = vector<ll>(n);
     for (auto &x : days)
         cin >> x;
-    for (int i = 1; i <= x + y; i++) {
-        //bool ans = f(i);
-        //cout << i << " " << ans << endl << endl;
-        if (f(i) == 1) {
-            cout << i << endl;
+    vector<ll>a = vector<ll>(n);
+    for (int i = 0; i < n; i++) {
+        a[i] = (days[i] - 1) % (x + y);
+    }
+    unordered_set<ll>st;
+    for (auto z : a)
+        st.insert(z);
+    for (auto i : st) {
+        if (f(i + 1) == 1) {
+            cout << i + 1 << endl;
             return 0;
         }
     }
     cout << -1 << endl;
     return 0;
 }
-
-/*
-
-3 3 1
-5 8 11
-
-*/
--- innopolisOpen/first_tour/c.cpp
+++ innopolisOpen/first_tour/c.cpp
@@ -6,7 +6,64 @@
 #define ll long long
 using namespace std;
 
+vector<int> right(vector<int>a) {
+    reverse(a.begin(), a.end());
+    stack<int>st;
+    stack<int>index;
+    st.push(-1);
+    index.push(-1);
+    vector<int>ans = vector<int>();
+    for (int i = 0; i < n; i++) {
+        while (st.top() >= a[i]) {
+            st.pop();
+            index.pop();
+        }
+        ans.push_back(index.top());
+        st.push(a[i]);
+        index.push(n - i - 1);
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+vector<int> left(vector<int>a) {
+    stack<int>st;
+    stack<int>index;
+    st.push(-1);
+    index.push(-1);
+    vector<int>ans = vector<int>();
+    for (int i = 0; i < n; i++) {
+        while (st.top() >= a[i]) {
+            st.pop();
+            index.pop();
+        }
+        ans.push_back(index.top());
+        st.push(a[i]);
+        index.push(i);
+    }
+    return ans;
+}
+
 int main() {
-    
+    int n, k;
+    cin >> n >> k;
+    vector<ll>a = vector<ll>(n);
+    for (auto &x : a)
+        cin >> x;
+    vector<ll>b = vector<ll>(n);
+    for (int i = 0; i < n; i++) {
+        if (a[i] < a[n - i - 1]) {
+            b[i] = a[n - i - 1] - a[i];
+        }
+    }
+    for (auto x : b)
+        cout << x << " ";
+    cout << endl;
+    while (true) {
+        ll sum = 0;
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
     return 0;
 }
