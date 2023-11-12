79d95752268699abc6b70474002583881772b5f0
DyhaCreator <andropet228@gmail.com>
2023-11-12 18:18:45 +0500

    new

--- tinkoff/perebor/e.cpp
+++ tinkoff/perebor/e.cpp
@@ -9,12 +9,27 @@ int a[100] = {0};
 
 void rec(int n, int pos) {
     if (n == 0) {
-        for 
+        for (int i = 0; i < nut; i++) {
+            cout << a[i] << " ";
+        } 
+        cout << endl;
+        return;
+    }
+    if (pos > 0) {
+        for (int i = n; i < a[pos - 1]; i++) {
+            a[pos] = i;
+            rec(n - 1, pos + 1);
+        }
+    } else {
+        for (int i = n; i <= k; i++) {
+            a[pos] = i;
+            rec(n - 1, pos + 1);
+        }
     }
 }
 
 int main() {
-    cin >> nut >> k;
+    cin >> k >> nut;
     rec(nut, 0);
     return 0;
 }
