2c36855d247a5dbac9b6bbd577a65a845aa8c25d
DyhaCreator <andropet228@gmail.com>
2023-11-19 02:12:09 +0500

    new

---
+++ tinkoff/sort/c.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+void sort(vector<int>&a) {
+    for (int i = 0; i < a.size(); i++) {
+        int max_index = i;
+        for (int j = i; j < a.size(); j++) {
+            if (a[j] > a[max_index]) {
+                max_index = j;
+            }
+        }
+        int buffer = a[i];
+        a[i] = a[max_index];
+        a[max_index] = buffer;
+    }
+    reverse(a.begin(), a.end());
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int>a = vector<int>(n);
+    for (auto &x : a)
+        cin >> x;
+    sort(a);
+    for (auto x : a)
+        cout << x << " ";
+    cout << endl;
+    return 0;
+}
