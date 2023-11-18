079204034eaca1f376e2346d04d5446211d70eee
DyhaCreator <andropet228@gmail.com>
2023-11-18 22:27:54 +0500

    new

--- lomonosov/e.cpp
+++ lomonosov/e.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 struct qw{
-    int l, r, t, v;
+    ll l, r, t, v;
     qw(){}
-    qw(int l, int r, int t, int v) {
+    qw(ll l, ll r, ll t, ll v) {
         this->l = l;
         this->r = r;
         this->t = t;
@@ -16,10 +16,10 @@ struct qw{
 };
 
 struct time_moment{
-    int t;
-    vector<int>moment;
+    ll t;
+    vector<ll>moment;
     time_moment() {}
-    time_moment(int t, vector<int>moment) {
+    time_moment(ll t, vector<ll>moment) {
         this->t = t;
         this->moment = moment;
     }
@@ -30,11 +30,11 @@ bool compt(qw a, qw b) {
 }
 
 int main() {
-    int k, n;
+    ll k, n;
     cin >> k >> n;
     vector<qw>q = vector<qw>();
-    for (int i = 0; i < n; i++) {
-        int l, r, t, v;
+    for (ll i = 0; i < n; i++) {
+        ll l, r, t, v;
         cin >> l >> r >> t >> v;
         qw a = qw(l - 1, r - 1, t, v);
         q.push_back(a);
@@ -45,11 +45,11 @@ int main() {
         cout << x.l << " " << x.r << " " << x.t << " " << x.v << endl;*/
 
     vector<time_moment>time_line = vector<time_moment>();
-    time_line.push_back(time_moment(-1, vector<int>(k)));
+    time_line.push_back(time_moment(-1, vector<ll>(k)));
 
     for (int i = 0; i < q.size(); i++) {
         if (q[i].t != time_line[time_line.size() - 1].t) {
-            vector<int>new_time = time_line[time_line.size()-1].moment;
+            vector<ll>new_time = time_line[time_line.size()-1].moment;
             new_time[q[i].l] += q[i].v;
             new_time[q[i].r + 1] -= q[i].v;
             time_line.push_back(time_moment(q[i].t, new_time));
@@ -67,7 +67,7 @@ int main() {
     vector<time_moment>out_time_line = vector<time_moment>(time_line.size());
 
     for (int i = 0; i < time_line.size(); i++) {
-        vector<int>moment = vector<int>(time_line[i].moment.size() + 1, 0);
+        vector<ll>moment = vector<ll>(time_line[i].moment.size() + 1, 0);
         for (int j = 1; j <= time_line[i].moment.size(); j++) {
             moment[j] = moment[j - 1] + time_line[i].moment[j - 1];
             //cout << moment[j] << " ";
@@ -80,7 +80,7 @@ int main() {
     vector<time_moment>pref_time_line = vector<time_moment>(out_time_line.size());
 
     for (int i = 0; i < out_time_line.size(); i++) {
-        vector<int>moment = vector<int>(out_time_line[i].moment.size(), 0);
+        vector<ll>moment = vector<ll>(out_time_line[i].moment.size(), 0);
         for (int j = 1; j < out_time_line[i].moment.size(); j++) {
             moment[j] = moment[j - 1] + out_time_line[i].moment[j];
             //cout << moment[j] << " ";
@@ -93,14 +93,26 @@ int main() {
     int m;
     cin >> m;
     for (int i = 0; i < m; i++) {
-        int l, r, t;
+        ll l, r, t;
         cin >> l >> r >> t;
         int index = 0;
-        while (index < pref_time_line.size() - 1 && pref_time_line[index].t < t) {
+        /*while (index < pref_time_line.size() - 1 && pref_time_line[index].t < t) {
             index++;
+        }*/
+        ll left = 0;
+        ll right = pref_time_line.size() - 1;
+        while (left < right) {
+            ll m = (left + right) / 2;
+            if (pref_time_line[m].t < t) {
+                left = m + 1;
+            } else {
+                right = m;
+            }
         }
+        index = right;
         cout << pref_time_line[index].moment[r] - pref_time_line[index].moment[l - 1] << endl; 
     }
-    printf("%.6f sec\n", (float)(clock() - start)/CLOCKS_PER_SEC);
+    //printf("%.6f sec\n", (float)(clock() - start)/CLOCKS_PER_SEC);
+    exit(0);
     return 0;
 }
