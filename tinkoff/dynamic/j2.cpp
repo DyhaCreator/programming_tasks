9b429a4342820eb6b2e5f68aa87929bbbf8e063b
DyhaCreator <andropet228@gmail.com>
2023-12-01 22:58:01 +0500

    new

--- tinkoff/dynamic/j2.cpp
+++ tinkoff/dynamic/j2.cpp
@@ -12,6 +12,12 @@ int main() {
             cin >> map[x][y];
         }
     }
-    
+    vector<vector<int>>a = vector<vector<int>>(n + 1, vector<int>(m + 1));
+    vector<vector<int>>b = vector<vector<int>>(n + 1, vector<int>(m + 1));
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if ()
+        }
+    }
     return 0;
 }
---
+++ tinkoff/dynamic/j3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>>map = vector<vector<int>>(n, vector<int>(m));
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < m; x++) {
+            cin >> map[y][x];
+        }
+    }
+    
+    return 0;
+}
