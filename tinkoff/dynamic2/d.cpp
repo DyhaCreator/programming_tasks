997495ff03a6cf4002184b3a1e25dba6d9f4783a
DyhaCreator <andropet228@gmail.com>
2023-12-10 16:54:43 +0500

    new

--- tinkoff/dynamic2/d.cpp
+++ tinkoff/dynamic2/d.cpp
@@ -5,10 +5,11 @@
 #define ll long long
 using namespace std;
 
-bool f(string str, int k) {
+int f(string str) {
     string l = str.substr(0, str.size() / 2);
     string r = str.substr((str.size() + 1) / 2, str.size());
     reverse(r.begin(), r.end());
+    cout << l << " " << r << endl;
     vector<vector<int>>a = vector<vector<int>>(l.size() + 1, vector<int>(r.size() + 1));
     for (int y = 1; y <= l.size(); y++) {
         for (int x = 1; x <= r.size(); x++) {
@@ -21,10 +22,8 @@ bool f(string str, int k) {
         }
         //cout << endl;
     }
-    if (l.size() - a[l.size()][r.size()] <= k) {
-        return true;
-    }
-    return false;
+    //cout << str.size() / 2 << " " << a[l.size()][r.size()] << endl;
+    return a[l.size()][r.size()];
 }
 
 int main() {
@@ -32,15 +31,18 @@ int main() {
     cin >> n >> k;
     string str = "";
     cin >> str;
+    //cout << str.size() << endl;
+    clock_t tStart = clock();
     int ans = 0;
     for (int i = 0; i < str.size(); i++) {
         for (int j = 1; j < str.size() - i + 1; j++) {
-            //cout << str.substr(i, j) << " " << endl;
-            if (f(str.substr(i, j), k)) {
+            cout << str.substr(i, j) << " " << endl;
+            if ((str.substr(i, j).size() / 2) - f(str.substr(i, j)) <= k) {
                 ans++;
             }
         }
     }
+    //printf("Time taken: %.4fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     cout << ans << endl;
     return 0;
 }
