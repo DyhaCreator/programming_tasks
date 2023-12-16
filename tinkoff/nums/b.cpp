5244dbb6e4512cb73bb1843e510f0e52ad043e7e
DyhaCreator <andropet228@gmail.com>
2023-12-16 18:27:42 +0500

    new

---
+++ tinkoff/nums/b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 2; i < sqrt(n); i++) {
+        
+    }
+    return 0;
+}
