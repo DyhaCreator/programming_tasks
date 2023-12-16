06569a45e40dafd411fc71322a1ced26f3449340
DyhaCreator <andropet228@gmail.com>
2023-12-16 19:25:38 +0500

    new

--- technoCup/a.cpp
+++ technoCup/a.cpp
@@ -4,5 +4,17 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>>a = vector<vector<int>>(n, vector<int>());
+    for (int i = 0; i < n; i++) {
+        int n1;
+        cin >> n1;
+        vector<int>a1 = vector<int>(n1);
+        for (auto &x : a1)
+            cin >> x;
+        a[i] = a1;
+    }
+    
     return 0;
 }
