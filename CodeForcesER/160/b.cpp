7f250f54c45a7b721a40e8e5078aa688fe2f6f4e
DyhaCreator <andropet228@gmail.com>
2023-12-18 19:58:35 +0500

    new

---
+++ CodeForcesER/160/b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        
+    }
+    return 0;
+}
