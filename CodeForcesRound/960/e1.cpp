9ade5fd9be415086b5e745c90bd3ec6a809c2a4b
DyhaCreator <andropet228@gmail.com>
2023-12-20 01:59:12 +0500

    ?????????????????

---
+++ CodeForcesRound/960/e1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        
+    }
+    return 0;
+}
