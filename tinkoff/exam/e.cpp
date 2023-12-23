2a7e28298de5819d8bc73d007d41f6d0a86543b1
DyhaCreator <andropet228@gmail.com>
2023-12-23 23:48:18 +0500

    ?????????????????????????,???

--- tinkoff/exam/e.cpp
+++ tinkoff/exam/e.cpp
@@ -15,9 +15,12 @@ ll gcd(int a, int b) {
 }
 
 int main() {
-    int a, b, c, i, j;
+    ll a, b, c, i, j;
     cin >> a >> b >> c >> i >> j;
-    ll l = gcd(gcd(a, b), c);
-    cout << l << endl; 
+    ll g1 = gcd(a, b);
+    ll g2 = gcd(a, c);
+    ll g3 = gcd(b, c);
+    ll nok = (a * b * c) / (g1 * g2 * g3);
+    cout << j / nok - i / nok << endl;
     return 0;
 }
