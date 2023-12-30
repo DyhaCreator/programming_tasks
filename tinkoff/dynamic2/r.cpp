3a48611b4e1a6be6b7f20eef4fb4dc4ceed8a750
DyhaCreator <andropet228@gmail.com>
2023-12-30 21:57:43 +0500

    ?????????????????????????,???????,??????????????

--- tinkoff/dynamic2/r.cpp
+++ tinkoff/dynamic2/r.cpp
@@ -1,15 +1,36 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #define ll long long
 using namespace std;
 
 int main() {
-    int a, k, b, m;
-    cin >> a >> k >> b >> m;
-    vector<int>a = vector<int>()
+    int n, a1, k, b, m;
+    cin >> n >> a1 >> k >> b >> m;
+    vector<int>a = vector<int>(n);
     a[0] = a1;
     for (int i = 1; i < n; i++) {
         a[i] = (k * a[i - 1] + b) % m;
+        //cout << a[i] << " ";
     }
+    //cout << endl;
+    vector<int>dp = vector<int>(n + 1);
+    dp[0] = -(m + 1);
+    for (int i = 1; i < n + 1; i++) {
+        dp[i] = m + 1;
+    }
+    for (int i = 0; i < n; i++) {
+        int j = int(upper_bound(dp.begin(), dp.end(), a[i]) - dp.begin());
+	    if (dp[j - 1] < a[i] && a[i] < dp[j]) {
+            dp[j] = a[i];
+        }
+    }
+    int i = 1;
+    while (dp[i] != m + 1) {
+        cout << dp[i] << " ";
+        i++;
+    }
+    cout << endl;
+    //cout << i - 1 << endl;
     return 0;
 }
