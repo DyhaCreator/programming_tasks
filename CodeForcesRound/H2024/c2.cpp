334af5951670e463c3c1e5eb0c7c32df9b927f57
DyhaCreator <andropet228@gmail.com>
2024-01-07 01:47:50 +0500

    ?????????????????????????,???????,????????,??????????????????????

---
+++ CodeForcesRound/H2024/c2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        vector<int>a = vector<int>(n);
+        for (auto &x : a)
+            cin >> x;
+        for (int i = 0; i < )
+    }
+    return 0;
+}
