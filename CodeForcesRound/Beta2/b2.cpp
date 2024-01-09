e2829fcfe2d99d7b53bee7d87a185c5c4755dfb0
DyhaCreator <andropet228@gmail.com>
2024-01-09 21:35:07 +0500

    ?????????????????????????,???????,????????,????????????????????????

--- CodeForcesRound/Beta2/b2.cpp
+++ CodeForcesRound/Beta2/b2.cpp
@@ -16,37 +16,37 @@ ll f(ll n) {
 int main() {
     int n;
     cin >> n;
-    vector<vector<ll>>map = vector<vector<ll>>(n, vector<ll>(n));
+    vector<vector<int>>map = vector<vector<int>>(n, vector<int>(n));
     for (int y = 0; y < n; y++) {
         for (int x = 0; x < n; x++) {
             cin >> map[y][x];
         }
     }
-    vector<vector<ll>>dp = vector<vector<ll>>(n + 1, vector<ll>(n + 1, 1));
+    vector<vector<int>>dp = vector<vector<int>>(n + 1, vector<int>(n + 1));
     vector<vector<char>>dp2 = vector<vector<char>>(n + 1, vector<char>(n + 1));
     for (int y = 1; y < n + 1; y++) {
         for (int x = 1; x < n + 1; x++) {
             if (y > 1 && x > 1) {
-                if (f(dp[y - 1][x] * map[y - 1][x - 1]) < f(dp[y][x - 1] * map[y - 1][x - 1])) {
-                    dp[y][x] = dp[y - 1][x] * map[y - 1][x - 1];
+                if (f(map[y - 1][x - 1] * map[y - 2][x - 1]) < f(map[y - 1][x - 1] * map[y - 1][x - 2])) {
+                    dp[y][x] = dp[y - 1][x] + f(map[y - 1][x - 1] * map[y - 2][x - 1]);
                     dp2[y][x] = 'D';
                 } else {
-                    dp[y][x] = dp[y][x - 1] * map[y - 1][x - 1];
+                    dp[y][x] = dp[y][x - 1] + f(map[y - 1][x - 1] * map[y - 1][x - 2]);
                     dp2[y][x] = 'R';
                 }
             } else if (y > 1) {
-                dp[y][x] = dp[y - 1][x] * map[y - 1][x - 1];
+                dp[y][x] = dp[y - 1][x] + f(map[y - 1][x - 1] * map[y - 2][x - 1]);
                 dp2[y][x] = 'D';
             } else if (x > 1) {
-                dp[y][x] = dp[y][x - 1] * map[y - 1][x - 1];
+                dp[y][x] = dp[y][x - 1] + f(map[y - 1][x - 1] * map[y - 1][x - 2]);
                 dp2[y][x] = 'R';
             } else {
-                dp[y][x] = map[y - 1][x - 1];
+                dp[y][x] = 0;
                 dp2[y][x] = '0';
             }
         }
     }
-    cout << f(dp[n][n]) << endl;
+    cout << dp[n][n] << endl;
     int y = n;
     int x = n;
     vector<char>ans = {};
@@ -63,6 +63,5 @@ int main() {
     for (auto x : ans)
         cout << x;
     cout << endl;
-    //cout << dp2[n][n] << endl;
     return 0;
 }
