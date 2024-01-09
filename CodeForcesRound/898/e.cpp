bf62da04ae01ade7a1b54e1bf7cf9f5b4e673ad7
DyhaCreator <andropet228@gmail.com>
2024-01-09 22:27:23 +0500

    ?????????????????????????,???????,????????,?????????????????????????????

---
+++ CodeForcesRound/898/e.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n, k;
+        cin >> n >> k;
+        vector<int>a = vector<int>(n);
+        for (auto &x : a)
+            cin >> x;
+        int ans = 0;
+        int w = 0;
+        while (w < k) {
+            for (int j = 0; j < n; j++) {
+                //cout << a[j] << " ";
+                if (a[j] < ans) {
+                    w++;
+                }
+            }
+            /*cout << endl;
+            cout << w << endl;*/
+            ans++;
+        }
+        cout << ans - 2 << endl;
+    }
+    return 0;
+}
