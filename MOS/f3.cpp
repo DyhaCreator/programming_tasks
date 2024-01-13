0b60b2717f614024838149a4e17be90bad15d6cf
DyhaCreator <andropet228@gmail.com>
2024-01-13 13:13:38 +0500

    ?????????????????????????,???????,????????,????????????????????????????????????????????????

---
+++ MOS/f3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int n, k, x;
+    cin >> n >> k >> x;
+    vector<int>a = vector<int>(n);
+    for (auto &x : a)
+        cin >> x;
+    
+    return 0;
+}
