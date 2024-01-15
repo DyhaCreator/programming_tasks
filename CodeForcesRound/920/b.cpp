eb9ff2d466f36fe1ded11fec25a709fd8a283503
DyhaCreator <andropet228@gmail.com>
2024-01-16 01:05:29 +0500

    ?????????????????????????,???????,????????,????????????????????????????????????????????????????????????????

--- CodeForcesRound/920/b.cpp
+++ CodeForcesRound/920/b.cpp
@@ -16,13 +16,20 @@ int main() {
         for (int j = 0; j < n; j++) {
             //cout << s[j] << " " << f[j] << endl;
             if (s[j] == '0' && f[j] == '1') {
-                ans++;
-            } else if (s[j] == '1' && f[j] == '0') {
-                if ((j > 0 && s[j - 1] == '0' && f[j - 1] == '1') || (j < n - 1 && s[j + 1] == '0' && f[j + 1] == '1')) {
-                    
+                if (j > 0 && s[j - 1] == '1' && f[j - 1] == '0') {
+                    s[j] = 0;
+                    f[j] = 0;
                 } else {
                     ans++;
                 }
+            } else if (s[j] == '1' && f[j] == '0') {
+                if (j > 0 && s[j - 1] == '0' && f[j - 1] == '1') {
+                    s[j] = 0;
+                    f[j] = 0;
+                } else {
+                    ans++;
+                }
+                //cout << "alf" << endl;
             }
             //cout << ans;
         }
