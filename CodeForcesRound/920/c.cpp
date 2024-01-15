7f2313a827cd5b5bd0083af7f31eed7c3f356d49
DyhaCreator <andropet228@gmail.com>
2024-01-16 00:53:46 +0500

    ?????????????????????????,???????,????????,???????????????????????????????????????????????????????????????

---
+++ CodeForcesRound/920/c.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n, f, a, b;
+        cin >> n >> f >> a >> b;
+        vector<int>m = vector<int>(n);
+        for (auto &x : m)
+            cin >> x;
+        
+    }
+    return 0;
+}
