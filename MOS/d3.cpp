9dd2c1be7fe090104bec4b28a4742d7e1ecae735
DyhaCreator <andropet228@gmail.com>
2024-01-15 18:49:56 +0500

    ?????????????????????????,???????,????????,????????????????????????????????????????????????????????

---
+++ MOS/d3.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+struct int2{
+    int x, y;
+    int2() {}
+    int2(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n, m, b;
+        cin >> n >> m >> b;
+        vector<vector<int>>map(n, vector<int>(m));
+        vector<int2>v(b);
+        for (int j = 0; j < b; j++) {
+            int x, y;
+            cin >> x >> y;
+            x--;
+            y--;
+            map[y][x] = 1;
+            v[j] = int2(x, y);
+        }
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1));
+        for (int y = 1; y <= n; y++) {
+            for (int x = 1; x <= m; x++) {
+                dp[y][x] = std::max(dp[y - 1][x], dp[y][x - 1]) + map[y - 1][x - 1];
+            }
+        }
+        //cout << dp[n][m] << endl;
+        int ans = 0;
+        for (int j = 0; j < b; j++) {
+            map[v[j].y][v[j].x] = 0;
+
+            vector<vector<int>>dp2(n + 1, vector<int>(m + 1));
+            for (int y = 1; y <= n; y++) {
+                for (int x = 1; x <= m; x++) {
+                    dp2[y][x] = std::max(dp2[y - 1][x], dp2[y][x - 1]) + map[y - 1][x - 1];
+                    //cout << map[y - 1][x - 1] << " ";
+                }
+                //cout << endl;
+            }
+            //cout << dp2[n][m] << endl;
+            if (dp2[n][m] < dp[n][m]) {
+                ans++;
+            }
+            map[v[j].y][v[j].x] = 1;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+/*
+
+1
+6 5 5
+5 5
+2 2
+3 3
+4 4
+1 1
+
+1
+4 4 5
+2 1
+3 1
+4 1
+1 2
+1 3
+
+*/
---
+++ tinkoff/scanline/b2.cpp
@@ -0,0 +1,114 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#define ll long long
+using namespace std;
+
+struct line{
+    int start;
+    int end;
+    line() {}
+    line(int start, int end) {
+        this->start = start;
+        this->end = end;
+    }
+};
+
+struct point{
+    int x, type;
+    point() {}
+    point(int x, int type) {
+        this->x = x;
+        this->type = type;
+    }
+};
+
+bool comp(const line &a, const line &b) {
+    if (a.start == b.start) {
+        return a.end < b.end;
+    } else {
+        return a.start < b.start;
+    }
+}
+
+bool comp2(const point &a, const point &b) {
+    if (a.x == b.x) {
+        if (a.type == 0 && b.type != 0) {
+            return false;
+        } else if (b.type == 0 && a.type != 0) {
+            return true;
+        } else if (a.type == 0 && b.type == 0) {
+            return true;
+        }
+    }
+    return a.x < b.x;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<line>lines(n);
+    for (auto &[a, b] : lines)
+        cin >> a >> b;
+    sort(lines.begin(), lines.end(), comp);
+    lines.push_back(line(1000000000, 1000000007));
+    vector<point>points(m);
+    for (auto &x : points) {
+        cin >> x.x;
+        x.type = 0;
+    }
+
+    int indexStart = 0;
+    int indexEnd = 0;
+
+    while (indexEnd < n) {
+        if (lines[indexStart].start < lines[indexEnd].end) {
+            points.push_back(point(lines[indexStart].start, 1));
+            indexStart++;
+        } else {
+            points.push_back(point(lines[indexEnd].end, -1));
+            indexEnd++;
+        }
+    }
+
+    sort(points.begin(), points.end(), comp2);
+
+    for (auto &x : points) {
+        cout << x.x << " " << x.type << endl;
+    }
+
+    int h = 0;
+    map<int, int>mp;
+
+    for (auto &x : points) {
+        h += x.type;
+        if (x.type == 0) {
+            cout << h << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
+/*
+
+5 1
+1 4
+6 10
+3 7
+2 5
+3 8
+3
+
+3 2
+0 5
+-3 2
+7 10
+1 6
+
+1 3
+-10 10
+-100 100 0
+
+
+*/
