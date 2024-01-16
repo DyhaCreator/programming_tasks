7770fe49ace228fb5d0a29030d369bcc9c87947c
DyhaCreator <andropet228@gmail.com>
2024-01-16 22:05:25 +0500

    ?????????????????????????,???????,????????,????????????????????????????????????????????????????????????????????????????????

--- tinkoff/scanline/d6.cpp
+++ tinkoff/scanline/d6.cpp
@@ -28,21 +28,28 @@ int main() {
         cin >> lines[i].l >> lines[i].r;
         lines[i].index = i;
     }
-    //clock_t tStart = clock();
+    clock_t tStart = clock();
     sort(lines.begin(), lines.end(), comp);
+
     vector<int>ans(n);
-    for (int i = 1; i < n; i++) {
-        int last = i - 1;
-        while (last >= 0 && !(lines[i].l >= lines[last].l && lines[i].r <= lines[last].r)) {
-            last--;
-        }
-        if (last >= 0) {
-            ans[lines[i].index] = lines[last].index + 1;
+
+    int max_end = 0;
+    int max_end_index = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (lines[i].r <= max_end) {
+            ans[lines[i].index] = max_end_index + 1;
+        } else {
+            max_end = lines[i].r;
+            max_end_index = lines[i].index;
+            ans[lines[i].index] = 0;
         }
     }
+
     for (auto &x : ans) {
         cout << x << " ";
     }
     cout << endl;
-    //printf("Time taken: %.6fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    printf("Time taken: %.6fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    return 0;
 }
