ae9bc92dfc6bdc11103f6efa51978ff11c89cbd6
DyhaCreator <andropet228@gmail.com>
2024-01-19 06:35:50 +0300

    ?????????????????????????,???????,????????,????????????????????????????????????????????????????????????????????????????????????????

---
+++ CodeForcesRound/CodeForcesER/161/b3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+struct int2{
+    int x, y;
+    int2() {}
+    int2(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        vector<int>a(n);
+        for (auto &x : a)
+            cin >> x;
+        vector<int2>p = vector<int2>();
+        int size = 1;
+        int last = a[0];
+        for (int j = 1; j < n; j++) {
+            if (a[j] == last) {
+                size++;
+            } else {
+                p.push_back(int2(last, size));
+                last = a[j];
+                size = 1;
+            }
+        }
+        
+    }
+    return 0;
+}
