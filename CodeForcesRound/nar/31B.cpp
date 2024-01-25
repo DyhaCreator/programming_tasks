a147ed13d24f531d05436d9b12d44c126cf56fce
DyhaCreator <andropet228@gmail.com>
2024-01-25 21:43:54 +0500

    new

---
+++ CodeForcesRound/nar/31B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#define ll long long
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    int n = int(str.size());
+    vector<string>ans = {};
+    
+    return 0;
+}
