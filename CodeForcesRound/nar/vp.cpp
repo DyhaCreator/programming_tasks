b6c55d02a12944de100bad8cbc70ae7017c28d1c
DyhaCreator <andropet228@gmail.com>
2024-01-25 16:59:44 +0500

    new

--- CodeForcesRound/nar/vp.cpp
+++ CodeForcesRound/nar/vp.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <vector>
 #define ll long long
 using namespace std;
 
+ll f(int n) {
+    return (1 << n) - 1;
+}
+
 int main() {
-    
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        ll x;
+        cin >> x;
+        vector<ll>a = {};
+        int min = 100;
+        x--;
+        while (x > 0) {
+            ll n = 1;
+            while (f(n) <= x) {
+                n++;
+            }
+            n--;
+            // cout << n << endl;
+            // vector<int>b = {};
+            for (int j = 0; j < n; j++) {
+                a.push_back(min + j - n);
+            }
+            min = min - n;
+            x -= f(n);
+        }
+        cout << a.size() << endl;
+        for (auto &x : a)
+            cout << x << " ";
+        cout << endl;
+    }
     return 0;
 }
