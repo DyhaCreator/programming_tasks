7cc3481a0aba01913a5579e3fce9babb79f9af52
DyhaCreator <andropet228@gmail.com>
2024-01-26 12:17:23 +0500

    new

---
+++ CodeForcesRound/nar/74B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#define ll long long
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    string str;
+    cin >> str >> str;
+    // cout << str << endl;
+    bool flagC;
+    if (str == "head") {
+        flagC = true;
+    } else {
+        flagC = false;
+    }
+    cin >> str;
+    int len = str.size();
+    for (int i = 0; i < len; i++) {
+        
+    }
+    return 0;
+}
