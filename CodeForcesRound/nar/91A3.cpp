cc4abcdc4d914f99b4b70292aad0441df8c93bcb
DyhaCreator <andropet228@gmail.com>
2024-01-26 14:23:38 +0500

    new

---
+++ CodeForcesRound/nar/91A3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#define ll long long
+using namespace std;
+
+int main() {
+    string str1, str2;
+    cin >> str1 >> str2;
+    return 0;
+}
