89b1ed75f90368a554186e509c2eae296a33c04f
DyhaCreator <andropet228@gmail.com>
2024-01-26 20:52:52 +0500

    new

--- CodeForcesRound/nar/1921C.cpp
+++ CodeForcesRound/nar/1921C.cpp
@@ -13,19 +13,26 @@ int main() {
         for (int i = 1; i <= n; i++) {
             cin >> v[i];
         }
-        for (auto &x : v)
+        /*for (auto &x : v)
             cout << x << " ";
-        cout << endl;
+        cout << endl;*/
         vector<int>dp(n + 1);
         dp[0] = v[1];
         dp[1] = v[2];
         for (int i = 2; i < n; i++) {
-            dp[i] = min(dp[i - 1] + (v[i] - v[i - 1]), dp[i - 2] + (v[i] - v[i - 1]));
+            // dp[i] = min(dp[i - 1] + (v[i] - v[i - 1]), dp[i - 2] + (v[i] - v[i - 1]));
+            dp[i] = std::min(dp[i - 1] + (v[i + 1] - v[i]) * a, dp[i - 2] + b);
             //cout << dp[i] << endl;
         }
-        for (auto &x : dp)
+        /*for (auto &x : dp)
             cout << x << " ";
         cout << endl;
+        cout << dp[n - 1] << endl;*/
+        if (dp[n - 1] >= f) {
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+        }
     }
     return 0;
 }
