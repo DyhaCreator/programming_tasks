820c670c10bf270321256e11b7fa1347821c4fa3
DyhaCreator <andropet228@gmail.com>
2024-01-31 01:25:17 +0500

    new

--- CodeForcesRound/922/c.cpp
+++ CodeForcesRound/922/c.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include <vector>
 #define ll long long
+#define ull long long
 using namespace std;
 
+ull f(ull a, ull b, ull r) {
+    ull ans = 1e18;
+    ull cash = 1;
+    for (int i = 0; i < 64; i++) {
+        if (cash <= r) {
+            ull fans = abs((a ^ cash) - (b ^ cash));
+            //cout << cash << " " << fans << endl;
+            if (fans < ans) {
+                ans = fans;
+            }
+            cash = cash << 1;
+        }
+    }
+    if (ans > abs(a - b)) {
+        ans = abs(a - b);
+    }
+    return ans;
+}
+
 int main() {
-    int t;
+    ll t;
     cin >> t;
     for (int i = 0; i < t; i++) {
-        ll a, b, r;
+        ull a, b, r;
         cin >> a >> b >> r;
-        
+        cout << f(a, b, r) << endl;
     }
     return 0;
 }
