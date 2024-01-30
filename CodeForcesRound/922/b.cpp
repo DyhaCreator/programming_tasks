3622f5b28aa99022f3430489bd507bfbdf3e060c
DyhaCreator <andropet228@gmail.com>
2024-01-31 01:10:02 +0500

    new

--- CodeForcesRound/922/b.cpp
+++ CodeForcesRound/922/b.cpp
@@ -1,7 +1,25 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #define ll long long
 using namespace std;
+
+struct int2{
+    int x, y;
+    int2() {}
+    int2(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+bool comp(const int2 &a, const int2 &b) {
+    if (abs(a.x - b.x) > abs(a.y - b.y)) {
+        return a.x < b.x;
+    } else {
+        return a.y < b.y;
+    }
+}
 
 int main() {
     int t;
@@ -9,13 +27,18 @@ int main() {
     for (int i = 0; i < t; i++) {
         int n;
         cin >> n;
-        vector<int>a(n);
-        vector<int>b(n);
-        for (auto &x : a)
-            cin >> x;
-        for (auto &x : b)
-            cin >> x;
-        
+        vector<int2>ab(n);
+        for (auto &x : ab)
+            cin >> x.x;
+        for (auto &x : ab)
+            cin >> x.y;
+        sort(ab.begin(), ab.end(), comp);
+        for (auto &x : ab)
+            cout << x.x << " ";
+        cout << endl;
+        for (auto &x : ab)
+            cout << x.y << " ";
+        cout << endl;
     }
     return 0;
 }
