929e78c4338c87de144e82949e6c38c9ad271975
DyhaCreator <andropet228@gmail.com>
2024-02-01 15:02:46 +0500

    new

--- myContest/testCon/a.cpp
+++ myContest/testCon/a.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main() {
+    ll n, a;
+    cin >> n >> a;
+    ll ans = 0;
+    ll x;
+    while (cin >> x) {
+        if (x >= a) {
+            ans++;
+            if (ans >= 4) {
+                printf("YES\n");
+                return 0;
+            }
+        }
+    }
+    printf("NO\n");
     return 0;
 }
