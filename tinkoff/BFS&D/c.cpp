f6f5e17384b91bd29cb3c591024825ac349492b5
DyhaCreator <andropet228@gmail.com>
2024-02-03 23:14:18 +0500

    new

--- tinkoff/BFS&D/c.cpp
+++ tinkoff/BFS&D/c.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <deque>
 #define ll long long
-#define INF -1
+#define INF 1000000007
 using namespace std;
 
 struct int2{
@@ -20,28 +20,32 @@ int n, m, s;
 vector<int> bfs01(int start) {
     int step = 0;
     vector<int> visited(n, INF);
-    deque<int> d;
+    deque<int2> d;
 
-    d.push_front(start);
+    d.push_back(int2(start, 0));
     visited[start] = step;
     /*for (auto &x : visited)
         cout << x << " ";
     cout << endl;*/
+    // int lastIndex = start;
     while (!d.empty()) {
-        int index = d.front();
+        int2 index = d.front();
         d.pop_front();
-
-        for (auto &x : g[index]) {
-            if (visited[x.x] == INF) {
+        // visited[index.x] = visited[lastIndex] + index.y;
+        /*for (auto &x : visited)
+            cout << x << " ";
+        cout << endl;*/
+        for (auto &x : g[index.x]) {
+            if (visited[x.x] > visited[index.x] + x.y) {
+                visited[x.x] = visited[index.x] + x.y;
                 if (x.y == 1) {
-                    d.push_back(x.x);
-                    visited[x.x] = visited[index] + 1;
+                    d.push_back(x);
                 } else {
-                    d.push_front(x.x);
-                    visited[x.x] = visited[index];
+                    d.push_front(x);
                 }
             }
         }
+        // lastIndex = index.x;
     }
 
     return visited;
@@ -56,11 +60,21 @@ int main() {
         g[a - 1].push_back(int2(b - 1, c));
         g[b - 1].push_back(int2(a - 1, c));
     }
+    /*for (int i = 0; i < n; i++) {
+        cout << i << endl;
+        for (auto &x : g[i]) {
+            cout << x.x << " " << x.y << endl;
+        }
+    }*/
 
-    vector<int> visited = bfs01(0);
+    vector<int> visited = bfs01(s - 1);
 
     for (auto &x : visited)
-        cout << x << " ";
+        if (x == INF) {
+            cout << -1 << " ";
+        } else {
+            cout << x << " ";
+        }
     cout << endl;
     return 0;
 }
