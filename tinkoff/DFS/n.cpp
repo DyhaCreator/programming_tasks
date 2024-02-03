dda87276f018cc68cc6062f3036a1f8c7a52b60d
DyhaCreator <andropet228@gmail.com>
2024-02-03 19:29:56 +0500

    new

--- tinkoff/DFS/n.cpp
+++ tinkoff/DFS/n.cpp
@@ -3,10 +3,23 @@
 #define ll long long
 using namespace std;
 
+int n, m;
 vector<vector<int>> g;
 
+void dfs(int start, vector<int> &visited, int sum) {
+    cout << start << endl;
+    visited[start] = sum;
+    /*for (auto &x : visited)
+        cout << x << " ";
+    cout << endl;*/
+    for (int i = 0; i < n; i++) {
+        if (g[start][i] != 0 && (visited[i] == -1 || visited[i] >= max(sum, g[start][i]))) {
+            dfs(i, visited, max(sum, g[start][i]));
+        }
+    }
+}
+
 int main() {
-    int n, m;
     cin >> n >> m;
 
     g = vector<vector<int>>(n, vector<int>(n));
@@ -18,13 +31,21 @@ int main() {
         g[b - 1][a - 1] = z;
     }
 
-    for (int y = 0; y < n; y++) {
+    int a, b;
+    cin >> a >> b;
+
+    /*for (int y = 0; y < n; y++) {
         for (int x = 0; x < n; x++) {
             cout << g[y][x] << " ";
         }
         cout << endl;
-    }
+    }*/
 
-    
+    vector<int> visited(n, -1);
+
+    dfs(a - 1, visited, 0);
+
+
+    cout << visited[b - 1] << endl;
     return 0;
 }
