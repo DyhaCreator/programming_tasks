63cb0f32cb53b71a6b0a706e962a99c7edf50a83
DyhaCreator <andropet228@gmail.com>
2024-02-08 21:21:56 +0500

    new

---
+++ CodeForcesRound/920/c2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n, f, a, b;
+        cin >> n >> f >> a >> b;
+
+        
+    }
+    return 0;
+}
