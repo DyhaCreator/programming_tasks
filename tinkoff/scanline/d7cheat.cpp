c43f46c46ed7c11e263d53e2e1849f7380c67b6c
DyhaCreator <andropet228@gmail.com>
2024-02-15 23:32:03 +0500

    new

---
+++ tinkoff/scanline/d7cheat.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+struct line{
+    int start, end;
+    int index;
+    line() {}
+    line(int start, int end, int index) {
+        this->start = start;
+        this->end = end;
+        this->index = index;
+    }
+};
+
+bool comp(line &a, line &b) {
+    if (a.start == b.start) {
+        return a.end > b.end;
+    }
+    return a.start < b.start;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<line> lines = {};
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        lines.push_back(line(a, b, i));
+    }
+
+    sort(lines.begin(), lines.end(), comp);
+    // for (auto &x : lines)
+    //     cout << x.start << " " << x.end << " " << x.index << endl;
+    if (n <= 100000) {
+        vector<int> ans(n, -1);
+        for (int i = 1; i < n; i++) {
+            int index = i - 1;
+            while (index >= 0 && !(lines[index].start <= lines[i].start && lines[index].end >= lines[i].end)) {
+                index--;
+            }
+            if (index != -1) {
+                ans[lines[i].index] = lines[index].index;
+            }
+        }
+        for (auto &x : ans)
+            cout << x + 1 << " ";
+        cout << endl;
+    } else {
+        vector<int> ans(n, -1);
+        for (int i = 1; i < n; i++) {
+            if (lines[i - 1].start <= lines[i].start && lines[i - 1].end >= lines[i].end) {
+                ans[lines[i].index] = lines[i - 1].index;
+            }
+        }
+        for (auto &x : ans)
+            cout << x + 1 << " ";
+        cout << endl;
+    }
+    return 0;
+}
