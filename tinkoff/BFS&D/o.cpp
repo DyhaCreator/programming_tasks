c59bc733b742c867ac208fb7799cb25859d3c668
DyhaCreator <andropet228@gmail.com>
2024-02-23 02:19:29 +0500

    new

---
+++ tinkoff/BFS&D/o.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    for (auto &x : c)
+        cin >> x;
+    int m;
+    cin >> m;
+    
+    return 0;
+}
--- tinkoff/scanline/e4.cpp
+++ tinkoff/scanline/e4.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <vector>
 #define ll long long
 #define INF 1000000007
@@ -16,7 +17,7 @@ struct line{
 
 bool comp(line &a, line &b) {
     if (a.end == b.end) {
-        return a.start < b.start;
+        return a.start > b.start;
     }
     return a.end < b.end;
 }
@@ -24,31 +25,40 @@ bool comp(line &a, line &b) {
 int main() {
     int n, k;
     cin >> n >> k;
+    // k = 1;
     vector<line> lines(n);
     for (auto &x : lines)
         cin >> x.start >> x.end;
     sort(lines.begin(), lines.end(), comp);
 
-    vector<int> ls(k, -1);
+    multiset<int> ms;
+    for (int i = 0; i < k; i++) {
+        ms.insert(0);
+    }
+
+    vector<int> v(k);
 
     int ans = 0;
 
     for (int i = 0; i < n; i++) {
-        int indexOfMin = 0;
-        for (int j = 0; j < k; j++) {
-            if (ls[j] < ls[indexOfMin]) {
-                indexOfMin = j;
-                if (ls[indexOfMin] <= lines[i].start) {
-                    ls[indexOfMin] = lines[i].end;
-                    ans++;
-                    break;
-                }
+        /*for ( auto &x : ms)
+            cout << x << " ";
+        cout << endl;*/
+        if (*ms.begin() <= lines[i].start) {
+            ms.erase(ms.begin());
+            ms.insert(lines[i].end);
+            ans++;
+        }
+        /*for (auto &x : v) {
+            if (x <= lines[i].start) {
+                ans++;
+                x = lines[i].end;
+                break;
             }
         }
-        /*if (ls[indexOfMin] <= lines[i].start) {
-            ans++;
-            ls[indexOfMin] = lines[i].end;
-        }*/
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;*/
     }
     cout << ans << endl;
     return 0;
