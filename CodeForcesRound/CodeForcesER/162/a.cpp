f2d0ed7acb242df77e09b8c3570192482c6fafed
DyhaCreator <andropet228@gmail.com>
2024-02-24 00:13:37 +0500

    new

--- CodeForcesRound/CodeForcesER/162/a.cpp
+++ CodeForcesRound/CodeForcesER/162/a.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #define ll long long
@@ -6,7 +7,7 @@
 #define all(vec) vec.begin(),vec.end()
 using namespace std;
 
-int main() {
+void solveA() {
     int t;
     cin >> t;
     for (int i = 0; i < t; i++) {
@@ -33,5 +34,57 @@ int main() {
         }
         cout << ans << endl;
     }
+}
+
+struct mon{
+    ll a, x;
+    mon() {}
+    mon(ll a, ll x) {
+        this->a = a;
+        this->x = x;
+    }
+};
+
+bool comp(mon &a, mon &b) {
+    return abs(a.x) < abs(b.x);
+}
+
+void solveB() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        ll n, k;
+        cin >> n >> k;
+        vector<mon> m(n);
+
+        for (auto &[a, x] : m)
+            cin >> a;
+        for (auto &[a, x] : m)
+            cin >> x;
+
+        sort(m.begin(), m.end(), comp);
+
+        for (ll j = 1; j < n; j++) {
+            m[j].a += m[j - 1].a;
+        }
+
+        /*for (auto &x : m)
+            cout << x.a << " ";
+        cout << endl;*/
+        bool f = false;
+        for (int j = 0; j < n; j++) {
+            //cout << (m[j].a + k - 1) / k << endl;
+            if ((m[j].a + k - 1) / k > abs(m[j].x)) {
+                cout << "NO" << endl;
+                f = true;
+                break;
+            }
+        }
+        if (!f) cout << "YES" << endl;
+    }
+}
+
+int main() {
+    solveB();
     return 0;
 }
