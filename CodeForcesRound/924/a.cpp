712fe5b329e4e592168dae1a6693aa04aa535fd0
DyhaCreator <andropet228@gmail.com>
2024-02-24 19:50:35 +0500

    new

--- CodeForcesRound/924/a.cpp
+++ CodeForcesRound/924/a.cpp
@@ -13,11 +13,59 @@ void solveA() {
     int t;
     cin >> t;
     for (int i = 0; i < t; i++) {
-        
+        int a, b;
+        cin >> a >> b;
+        bool find = false;
+        if (a % 2 == 0 && b != a / 2) {
+            find = true;
+        }
+        if (b % 2 == 0 && a != b / 2) {
+            find = true;
+        }
+        if (find) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+}
+
+void solveB() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        set<int> st;
+        for (int j = 0; j < n; j++) {
+            int x;
+            cin >> x;
+            st.insert(x);
+        }
+
+        vector<int> vec = {};
+        for (auto &x : st)
+            vec.push_back(x);
+
+        /*for (auto &x : vec)
+            cout << x << " ";
+        cout << endl;*/
+        int ans = 0;
+        int l = 0;
+        for (int r = 0; r < vec.size(); r++) {
+            while (vec[r] - vec[l] + 1 > n) {
+                l++;
+            }
+            if (vec[r] - vec[l] + 1 <= n) {
+                ans = std::max(r - l + 1, ans);
+                // cout << l << " " << r << endl;
+            }
+        }
+        cout << ans << endl;
     }
 }
 
 int main() {
-    solveA();
+    solveB();
     return 0;
 }
