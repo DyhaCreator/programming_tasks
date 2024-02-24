1a6d543305bfbed56f8673d322bed974faacd4ea
DyhaCreator <andropet228@gmail.com>
2024-02-24 17:15:48 +0500

    new

--- CodeForcesRound/925/a.cpp
+++ CodeForcesRound/925/a.cpp
@@ -102,7 +102,66 @@ void solveC() {
     }
 }
 
+void solveD() {
+    int t;
+    cin >> t;
+    for (int ii = 0; ii < t; ii++) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        vector<int> mp(n);
+        for (auto &x : mp)
+            cin >> x;
+        int ans = 0;
+        for (int y = 0; y < n - 1; y++) {
+            for (int x = y + 1; x < n; x++) {
+                if (abs(mp[y] + mp[x]) % a == 0 && abs(mp[y] - mp[x]) % b == 0) {
+                    ans++;
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+}
+
+void solveE() {
+    int t;
+    cin >> t;
+    for (int ii = 0; ii < t; ii++) {
+        int n, m;
+        cin >> n >> m;
+        vector<string> vec(n);
+        for (auto &x : vec)
+            cin >> x;
+        int indexA = 0;
+        for (int i = 0; i < n - 1; i++) {
+            while (indexA < n && vec[indexA][vec[indexA].size() - 1] != 0) {
+                indexA++;
+            }
+
+            if (indexA == n) indexA--;
+            cout << indexA << endl;
+
+            string str = vec[indexA];
+            // cout << str << endl;
+            int j = 1;
+            while (str[n - j] == '0') {
+                j++;
+            }
+            
+            string newStr = "";
+            for (int k = 0; k < n - j; k++) {
+                newStr.push_back(str[k]);
+            }
+            vec[indexA] = newStr;
+
+
+            string el1 = vec[0], el2 = vec[1];
+            
+        }
+    }
+}
+
 int main() {
-    solveC();
+    solveE();
     return 0;
 }
