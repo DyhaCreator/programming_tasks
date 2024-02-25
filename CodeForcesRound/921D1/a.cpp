6a2739c7fba8884d48613b78a88add60053e443e
DyhaCreator <andropet228@gmail.com>
2024-02-25 19:10:06 +0500

    new

---
+++ CodeForcesRound/921D1/a.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <set>
+#define ll long long
+#define INF 1000000007
+#define prvec(vec) for(auto &x:vec)cout<<x<<" ";cout<<endl;
+#define all(vec) vec.begin(),vec.end()
+#define envec(vec) for(auto &x:vec)cin>>x;
+using namespace std;
+
+void solveA() {
+    int t;
+    cin >> t;
+    for (int ii = 0; ii < t; ii++) {
+        
+    }
+}
+
+int main() {
+
+    return 0;
+}
