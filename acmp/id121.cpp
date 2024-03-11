aeb08f94b5175e290f847a728e7c5afcbbca5583
DyhaCreator <andropet228@gmail.com>
2024-03-12 02:22:41 +0500

    new

---
+++ acmp/id121.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a)
+        cin >> x;
+    vector<int> dp(n);
+    for (int i = 0; i < n; i++) {
+        
+    }
+    return 0;
+}
---
+++ acmp/id554.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    return 0;
+}
