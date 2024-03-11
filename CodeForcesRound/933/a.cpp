83340b6daa201bd9451da2cb9481f09614b944be
DyhaCreator <andropet228@gmail.com>
2024-03-12 01:35:14 +0500

    new

--- CodeForcesRound/933/a.cpp
+++ CodeForcesRound/933/a.cpp
@@ -32,7 +32,58 @@ void solveA() {
     }
 }
 
+void solveB() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (auto &x : a)
+            cin >> x;
+        if (a[0] * 2 > a[1] || a[a.size() - 1] * 2 > a[a.size() - 2]) {
+            cout << "NO" << endl;
+            continue;
+        }
+        int m = 0;
+        for (int i = 0; i < n; i++) {
+            m = max(m, a[i]);
+        }
+        int countMax = 0;
+        for (int i = 1; i < n; i++) {
+            
+        }
+    }
+}
+
+void solveC() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        int ans = 0;
+        for (int i = 0; i < n - 2; i++) {
+            if (str[i] == 'm' && str[i + 1] == 'a' && str[i + 2] == 'p') {
+                if (i < n - 4 && str[i + 3] == 'i' && str[i + 4] == 'e') {
+                    ans++;
+                    i += 4;
+                } else {
+                    ans++;
+                    i += 2;
+                }
+            } else if (str[i] == 'p' && str[i + 1] == 'i' && str[i + 2] == 'e') {
+                ans++;
+                i += 2;
+            }
+        }
+        cout << ans << endl;
+    }
+}
+
 int main() {
-    solveA();
+    solveC();
     return 0;
 }
