c82f3d803e21f5b6a7cf62fde495a8390313298a
DyhaCreator <andropet228@gmail.com>
2024-06-10 16:58:12 +0500

    new

---
+++ sirius/b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int>dp(n + 1);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i < n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    /*for (auto &x : dp)
+        cout << x << " ";
+    cout << endl;*/
+    cout << dp[n - 1] << endl;
+    return 0;
+}
