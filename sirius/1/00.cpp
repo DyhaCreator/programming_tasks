b4c658b20c8bbcdfe4d3bc23948b8bf8ed2725ac
DyhaCreator <andropet228@gmail.com>
2024-07-19 23:37:45 +0500

    new

--- sirius/1/00.cpp
+++ sirius/1/00.cpp
@@ -8,27 +8,24 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<ll> vec;
-    for (ll i = 0; i <= n; i++) {
-        vec.push_back(i * i);
-    }
-    for (ll i = 0; i * i * i <= n * n; i++) {
-        vec.push_back(i * i * i);
-    }
-    sort(vec.begin(), vec.end());
-    /*for (auto &x : vec)
-        cout << x << " ";
-    cout << endl;*/
-    int index = 0;
-    for (int i = 0; i < vec.size(); i++) {
-        //cout << vec[i] << " ";
-        if (index == n) {
-            cout << vec[i] << endl;
-            break;
+    vector<ll> a;
+    ll i1 = 0;
+    ll i2 = 0;
+    int size = 0;
+    while (size < n) {
+        if (i1 * i1 < i2 * i2 * i2) {
+            //a.push_back(i1 * i1);
+            i1++;
+        } else if (i1 * i1 > i2 * i2 * i2) {
+            //a.push_back(i2 * i2 * i2);
+            i2++;
+        } else {
+            //a.push_back(i1 * i1);
+            i1++;
+            i2++;
         }
-        index++;
-        if (vec[i] == vec[i + 1]) i++;
+        size++;
     }
-    cout << endl;
+    cout << min(i1 * i1, i2 * i2 * i2) << endl;
     return 0;
 }
