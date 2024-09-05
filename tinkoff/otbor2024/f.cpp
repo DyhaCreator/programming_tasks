e5035f89a4bab7c731dcb400eee3fe4d2717187e
DyhaCreator <andropet228@gmail.com>
2024-09-06 01:55:51 +0500

    new

--- tinkoff/otbor2024/f.cpp
+++ tinkoff/otbor2024/f.cpp
@@ -6,11 +6,47 @@ using ll = long long;
 const int INF = 1e9 + 7;
 
 int main() {
-    int n, p, q, r;
+    ll n, p, q, r;
     cin >> n >> p >> q >> r;
-    vector<int> a(n);
+    vector<ll> a(n);
     for (auto &x : a)
         cin >> x;
-    
+    ll j = 0;
+    ll sum = 0;
+    for (ll i = 0; i < n; i++) {
+        sum += a[i];
+        while (sum > p + q + r) {
+            sum -= a[j];
+            j++;
+        }
+        if (sum == p + q + r) {
+            // cout << j << " " << i << endl;
+            ll sum_p = 0;
+            ll sum_q = 0;
+            ll sum_r = 0;
+            ll index = j;
+            while (sum_p < p) {
+                sum_p += a[index];
+                index++;
+            }
+            if (sum_p == p) {
+                while (sum_q < q) {
+                    sum_q += a[index];
+                    index++;
+                }
+                if (sum_q == q) {
+                    while (sum_r < r && index <= i) {
+                        sum_r += a[index];
+                        index++;
+                    }
+                    if (sum_r == r) {
+                        cout << "YES" << endl;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+    cout << "NO" << endl;
     return 0;
 }
