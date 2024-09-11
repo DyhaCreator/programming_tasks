b59a14d7c6ce9345f3d92b101c2ebd25a4eac2fe
DyhaCreator <andropet228@gmail.com>
2024-09-11 17:16:08 +0500

    new

---
+++ inform/2/a_graph.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+const int INF = 1e9 + 7;
+
+vector<vector<int>> g;
+vector<int> sys = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int gen(int day, int month) {
+
+}
+
+int main() {
+    g = vector<vector<int>>(366, vector<int>());
+    gen(1, 1);
+
+    return 0;
+}
