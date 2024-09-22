94d2db302488b9517c6c063ac4c4dcc46f965f0f
DyhaCreator <andropet228@gmail.com>
2024-09-22 16:46:11 +0500

    new

---
+++ CodeForcesRound/nar/2009A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+const int INF = 1e9 + 7;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << (0 - a) + (b - 0) << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
