c5d5377e0631448fe1b693ec30b62e884b5feb9d
DyhaCreator <andropet228@gmail.com>
2024-10-14 23:15:30 +0500

    new

--- CodeForcesRound/nar/1984C1.cpp
+++ CodeForcesRound/nar/1984C1.cpp
@@ -11,16 +11,25 @@ void solve() {
     vector<ll> a(n);
     for (auto &x : a)
         cin >> x;
-    ll ans = 0;
-    for (auto &x : a) {
-        if (x > 0) {
-            ans = abs(ans) + x;
-        } else {
-            ans += x;
-        }
-        cout << ans << ' ';
+    vector<ll> dp1(n);
+    vector<ll> dp2(n);
+    dp1[0] = a[0];
+    dp2[0] = abs(a[0]);
+    for (int i = 1; i < n; i++) {
+        ll c1 = dp1[i - 1] + a[i];
+        ll c2 = abs(dp1[i - 1] + a[i]);
+        ll c3 = dp2[i - 1] + a[i];
+        ll c4 = abs(dp2[i - 1] + a[i]);
+        dp1[i] = min(min(c1, c2), min(c3, c4));
+        dp2[i] = max(max(c1, c2), max(c3, c4));
     }
-    cout << abs(ans) << endl;
+    /*for (auto &x : dp1)
+        cout << x << " ";
+    cout << endl;
+    for (auto &x : dp2)
+        cout << x << " ";
+    cout << endl;*/
+    cout << max(abs(dp1[n - 1]), abs(dp2[n - 1])) << endl;
 }
 
 int main() {
