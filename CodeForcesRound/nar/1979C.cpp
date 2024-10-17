51048e061b6603c8d3d3b3f9edddd6c907717242
DyhaCreator <andropet228@gmail.com>
2024-10-17 17:50:52 +0500

    new

--- CodeForcesRound/nar/1979C.cpp
+++ CodeForcesRound/nar/1979C.cpp
@@ -12,46 +12,19 @@ void solve() {
     vector<ll> a(n);
     for (auto &x : a)
         cin >> x;
-    int sum1 = 0;
-    set<int> st;
-    for (auto &x : a)
-        st.insert(x);
-    for (auto &x : st)
-        cout << x << " ";
-    cout << endl;
-    for (auto &x : st)
-        sum1 += x;
-    cout << (sum1 + st.size() - 1) / st.size() << endl;
-    if ((sum1 + st.size() - 1) / st.size() <= n) {
-        cout << -1 << endl;
-        return;
+    vector<ll> b(n);
+    ll sum = 0;
+    for (int i = 0; i < n; i++) {
+        b[i] = (INF + a[i] - 1) / a[i];
+        sum += b[i];
     }
-    vector<ll> b(n, 1);
-    ll c = 1;
-    ll sum = n;
-    ll count = 0;
-    while (c > 0) {
-        c = 0;
-        for (int i = 0; i < n; i++) {
-            if (sum >= b[i] * a[i]) {
-                ll sum2 = sum - b[i];
-                b[i] = (sum + a[i]) / a[i];
-                sum = sum2 + b[i];
-                c++;
-            }
-        }
-        if (count > n * n * n) {
-            cout << -1 << endl;
-            return;
-        }
+    if (sum < INF) {
         for (auto &x : b)
             cout << x << " ";
         cout << endl;
-        count++;
+    } else {
+        cout << -1 << endl;
     }
-    for (auto &x : b)
-        cout << x << " ";
-    cout << endl;
 }
 
 int main() {
