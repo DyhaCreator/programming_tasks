0e72e6f10514e93549e74c2a7d748e5ec8227ee4
DyhaCreator <andropet228@gmail.com>
2024-10-22 14:15:33 +0500

    new

---
+++ inform/5cpp/h.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+const int INF = 1e9 + 7;
+
+int main() {
+    string s;
+    getline(cin, s);
+    int f = 0;
+    char last = ']';
+    for (auto &x : s) {
+        if (x == ' ' && last != ' ') {
+            f++;
+        }
+        last = x;
+    }
+    stringstream in(s);
+    string str;
+    in >> str;
+    in >> str;
+    if (f == 1) {
+        str = " ";
+    }
+    cout << str << endl;
+    return 0;
+}
