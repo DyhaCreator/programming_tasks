0162c350e0fc9cea126c9d7901e1b4806e82f99a
DyhaCreator <andropet228@gmail.com>
2025-01-03 19:11:05 +0500

    new

--- CodeForcesRound/nar/graph/1437D.cpp
+++ CodeForcesRound/nar/graph/1437D.cpp
@@ -13,18 +13,28 @@ void solve() {
     for (auto &x : a)
         cin >> x;
     vector<int> b = {};
-    int cnt = 0;
+    int cnt = 1;
     for (int i = 2; i < n; i++) {
         if (a[i - 1] < a[i]) {
             cnt++;
         } else {
             b.push_back(cnt);
-            cnt = 0;
+            cnt = 1;
         }
     }
-    for (auto &x : b)
-        cout << x << " ";
-    cout << endl;
+    b.push_back(cnt);
+    vector<int> pref(b.size() + 1);
+    for (int i = 0; i < b.size(); i++)
+        pref[i + 1] = pref[i] + b[i];
+    
+    vector<int> c = {1};
+    int index = 0;
+    while (index < b.size()) {
+        c.push_back(pref[min(index + c.back(), int(b.size()))] - pref[index]);
+        index += c[c.size() - 2];
+    }
+    
+    cout << c.size() - 1 << endl;
 }
 
 int main() {
