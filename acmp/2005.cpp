52ba4c02bb5d6c0792c8bec0be3b21d7036b3a48
DyhaCreator <andropet228@gmail.com>
2025-01-04 16:25:02 +0500

    new

---
+++ acmp/2005.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+const int INF = 1e9 + 7;
+
+int main() {
+    ll n;
+    cin >> n;
+    cout << ((ll)((n + 1) / 2) * (ll)((n + 1) / 2)) << endl;
+    return 0;
+}
