34d1ccd14854811f719fb901fc195b4c55738f8f
DyhaCreator <andropet228@gmail.com>
2025-02-09 19:49:05 +0500

    new

---
+++ CodeForcesRound/nar/2065C.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+const int INF = 1e9 + 7;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    vector<int> b(m);
+    for (auto &x : a)
+        cin >> x;
+    for (auto &x : b)
+        cin >> x;
+    
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
