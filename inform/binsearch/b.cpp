a52dbffdaa78ac579fb95bfa6a620fcdfcd34030
DyhaCreator <andropet228@gmail.com>
2025-02-13 14:09:10 +0500

    new

---
+++ inform/binsearch/b.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+const int INF = 1e9 + 7;
+
+int main() {
+    ll k;
+    cin >> k;
+    
+    return 0;
+}
